4cff239ff8596edde1b5f736dd2ffa13a9012fa4
Moses Okode <okodekings76@gmail.com>
2024-12-06 01:02:01 +0300

    create the sign in form page

---
+++ src/Component/Form/SignIn.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './SignIn.css';
+import TatusImage from './Tatus.jpeg'; // Import the image
+
+export default function SignIn() {
+  return (
+    <div className="signin-modal">
+      <div className="signin-wrapper" style={{ backgroundImage: `url(${TatusImage})` }}>
+        <form>
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <i className="bx bxs-user"></i>
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <i className="bx bxs-lock-alt"></i>
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" /> Remember me
+            </label>
+            <a href="#">Forgot password?</a>
+          </div>
+          <button type="submit" className="btn">
+            Login
+          </button>
+          <div className="register-link">
+            <p>
+              Don't have an account? <a href="#">Register</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignIn from './Form/SignIn'; // Adjust path based on your folder structure
 
-export default function Hero(){
-    return (
-        <section className='section1'>
-            <nav className='navbar'>
-                <div className='logo'>
-                    <img src='/logo192.png' alt='logo' className='logo' />
-                </div>
-                <ul>
-                <li>About</li>
-                <li>Technology</li>
-                <li>Contact Us</li>
-                <button className='button'>Sign In</button>
-                <button className='button'>Sing Up</button>
-                </ul>
-            </nav>
-        </section>
-    )
+export default function Hero() {
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  const handleSignInClick = () => {
+    setShowSignIn((prev) => !prev);
+  };
+
+  return (
+    <section className="section1">
+      <nav className="navbar">
+        <div className="logo">
+          <img src="/logo192.png" alt="logo" />
+        </div>
+        <ul>
+          <li>About</li>
+          <li>Technology</li>
+          <li>Contact Us</li>
+          <button className="button" onClick={handleSignInClick}>
+            {showSignIn ? 'Close' : 'Sign In'}
+          </button>
+          <button className="button">Sign Up</button>
+        </ul>
+      </nav>
+      {/* Render SignIn form conditionally */}
+      {showSignIn && <SignIn />}
+    </section>
+  );
 }
-
