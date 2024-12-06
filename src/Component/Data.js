9a1b698a28701ca02ceaaa43c0cc913151f049cc
Moses Okode <okodekings76@gmail.com>
2024-12-07 01:39:32 +0300

    modified Data.js @ avail

--- src/Component/Data.js
+++ src/Component/Data.js
@@ -4,28 +4,28 @@ const data = [
       img: "/Images/darkbed.jpeg", // Use absolute path
       Rate: "4.9",
       para: "North Wing Hotel",
-      avail: 90
+      avail: 100
     },
     {
       id:2,
       img: "/Images/purplebed.jpeg",
       Rate: "5.0",
       para: "South Wing Hotel",
-      avail: 1
+      avail: 3
     },
     {
       id:3,
       img: "/Images/roombed.jpeg",
       Rate: "4.5",
       para: "East Wing Hotel",
-      avail: 2
+      avail: 35
     },
     {
       id:4,
       img: "/Images/beachbed.jpeg",
       Rate: "4.8",
       para: "West Wing Hotel",
-      avail: 9
+      avail: 90
     },
     {
         id:5,
